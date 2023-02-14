e17c95af4cdf8b1f8c560e7445701ab14ad16c7d
Fabrizio Ferrara <fzferrara@gmail.com>
2023-02-14 01:54:33 +0100

    Avanzando con el Main

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,7 +5,12 @@ from django.db import models
 class Curso(models.Model):
 
     nombre = models.CharField(max_length=40)
-    camada = models.IntegerField()
+    camada = models.IntegerField() 
+    
+    def __str__(self):
+        return self.nombre +'(' + str(self.camada) + ')'
+
+    
 
 class Estudiante(models.Model):
 
@@ -19,6 +24,9 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
+    
+    class Meta:
+        verbose_name_plural = 'Profesores'
 
 class Entregable(models.Model):
     
